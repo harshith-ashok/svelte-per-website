0c7767a2b9692693f78e35110dd8ee4d173e8c9d
Harshith Ashok <harshith.7k10@gmail.com>
2024-05-10 23:08:41 +0530

    v3 init

---
+++ src/routes/pics/+page.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+	<title>Photography</title>
+</svelte:head>
+
+<section class="p-20">
+	<h1><span class="text-4xl font-black font-mont normal">Photography</span></h1>
+	<p class="text-md leading-loose font-space py-5">I love taking photographs of scenories and the places i visit. I'm fascinated by nature and love wildlife.<br>I usually take photos on my <b>iPhone XS Max</b> but I am learning to use a dslr camera and few of the photos I take using my <b>Sony EOS 450D</b> with a 120mm lens</p>
+	<div class="grid grid-cols-1 gap-4 xl:grid-cols-4 md:grid-cols-2">
+		<img
+			src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaPZXz8RKCNF3a8dUvQGY_lRlbz23O7aqZ1oqKP2ha8lLGYKl31dGWFJKHPl5l_FH6ymIET_4xTLe9ku6LxkWhPK8jUwII4Pag=w945-h892-rw-v1"
+			alt="Shoes"
+			class="rounded-xl h-[500px]"
+		/>
+		<img
+			src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaPZXz8RKCNF3a8dUvQGY_lRlbz23O7aqZ1oqKP2ha8lLGYKl31dGWFJKHPl5l_FH6ymIET_4xTLe9ku6LxkWhPK8jUwII4Pag=w945-h892-rw-v1"
+			alt="Shoes"
+			class="rounded-xl h-[500px]"
+		/>
+		<img
+			src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaPZXz8RKCNF3a8dUvQGY_lRlbz23O7aqZ1oqKP2ha8lLGYKl31dGWFJKHPl5l_FH6ymIET_4xTLe9ku6LxkWhPK8jUwII4Pag=w945-h892-rw-v1"
+			alt="Shoes"
+			class="rounded-xl h-[500px]"
+		/>
+		<img
+			src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaPZXz8RKCNF3a8dUvQGY_lRlbz23O7aqZ1oqKP2ha8lLGYKl31dGWFJKHPl5l_FH6ymIET_4xTLe9ku6LxkWhPK8jUwII4Pag=w945-h892-rw-v1"
+			alt="Shoes"
+			class="rounded-xl h-[500px]"
+		/>
+	</div>
+</section>
